6e1dbef750610a995dad60a03ffc317cd6077424
Ananth Bhaskararaman <antsub@gmail.com>
2024-03-21 01:46:49 +0530

    fix: Extract styles from js to make burg menu work

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,18 @@
+function jsonExtractor(content) {
+  const els = JSON.parse(content).htmlElements;
+  return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
+}
+
 export default {
   plugins: {
     '@fullhuman/postcss-purgecss': {
-      content: ["./hugo_stats.json"],
-      defaultExtractor: (content) => {
-        const els = JSON.parse(content).htmlElements;
-        return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
-      },
+      content: ["./hugo_stats.json", "./assets/js/**/*.js"],
+      extractors: [
+        {
+          extractor: jsonExtractor,
+          extensions: ["json"]
+        }
+      ],
       safelist: [],
     },
     cssnano: { preset: 'default' }
